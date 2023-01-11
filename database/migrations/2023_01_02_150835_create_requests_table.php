b8a4efed6c004b9809e97a42a2973f7a94fe06a3
leleleleilel <markeuuu8@gmail.com>
2023-01-11 10:39:54 +0700

    make note request nullable

--- database/migrations/2023_01_02_150835_create_requests_table.php
+++ database/migrations/2023_01_02_150835_create_requests_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string('location');
             $table->tinyInteger('batch');
             $table->timestamp('deadline')->nullable();
-            $table->text('note');
+            $table->text('note')->nullable();
             $table->string('status');
             $table->timestamps();
             $table->softDeletes();
